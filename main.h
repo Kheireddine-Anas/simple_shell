33af2b870b053995c5a4f94a391e05fe5a573b74
Abdofer <abderrazaq.fermaq@gmail.com>
2023-05-24 11:07:55 -0700

    eeee

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+
+
+#endif
